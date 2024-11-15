6b61696b88c7c077b78a2e808e0084101837df94
Samuel Raducan <samuel.thembp@gmail.com>
2024-11-15 16:47:06 +0000

    Add POST endpoint for creating new TODO items

--- backend/src/repository/todo.js
+++ backend/src/repository/todo.js
@@ -1,12 +1,14 @@
-
 let todoList = {
-  todos: [
-    {
-      "task": "This is a todo example"
-    }
-  ]
+    todos: [
+        {
+            task: "This is a todo example",
+        },
+    ],
 };
 
 module.exports = {
-  getTodos: () => Promise.resolve(todoList)
+    getTodos: () => Promise.resolve(todoList),
+    addTodo: (newTodo) => {
+        todoList.todos.push(newTodo);
+    },
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const repository = require('./repository/todo');
-const todoService = require('./service/todo')(repository);
+const express = require("express");
+const cors = require("cors");
+const repository = require("./repository/todo");
+const todoService = require("./service/todo")(repository);
 
 const server = () => {
-  const server = express();
-  server.use(express.json());
-  server.use(cors());
+    const server = express();
+    server.use(express.json());
+    server.use(cors());
 
-  server.get('/api/todo', async (req, res) => {
-    res.json(await todoService.getTodos());
-  });
+    server.get("/api/todo", async (req, res) => {
+        res.json(await todoService.getTodos());
+    });
 
-  /**
+    server.post("/api/todo", async (req, res) => {
+        const newTodo = req.body;
+
+        repository.addTodo(newTodo);
+        res.json(await todoService.getTodos());
+    });
+    /**
   POST /api/todo
   {
    "task": "Some API"
@@ -27,6 +33,6 @@ const server = () => {
    }
   **/
 
-  return server;
+    return server;
 };
 module.exports = server;
